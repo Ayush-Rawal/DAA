618cdf4c5934a0bc3a48dd14382a7e2a61a7a275
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2019-03-26 10:46:22 +0530

    Added greedy max ratio for 0/1 knapsack

--- 3-26.cpp
+++ 3-26.cpp
@@ -10,8 +10,29 @@ struct item {
     item(int b, int w): benefit(b), weight(w) {}
 };
 
+
 int greedyMaxWeight(vector<item> items, int maxWeight) {
-    auto cmp = [](item left, item right) {return ((left.weight) >= (right.weight));};
+    auto cmp = [](item left, item right) {return ((left.weight) <= (right.weight));};
+    priority_queue<item, vector<item>, decltype(cmp)> q(cmp);
+
+    for(item i : items) {
+        q.push(i);
+    }
+
+    int currWeight = 0, currBenefit = 0;    
+    while(!q.empty()) {
+        if(currWeight + q.top().weight <= maxWeight) {
+            currWeight += q.top().weight;
+            currBenefit += q.top().benefit;
+        }
+        q.pop();
+    }
+
+    return currBenefit;
+}
+
+int greedyMaxRatio(vector<item> items, int maxWeight) {
+    auto cmp = [](item left, item right) {return ((left.benefit/left.weight) <= (right.benefit/right.weight));};
     priority_queue<item, vector<item>, decltype(cmp)> q(cmp);
 
     for(item i : items) {
